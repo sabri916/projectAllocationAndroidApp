6fa25848e420c2326d22aba486a4755dc594215b
Sabri K <sbrkrs@gmail.com>
2016-05-30 15:47:38 +0400

    dialog crashing D=

--- app/src/main/java/om/gov/ita/drawerbottomnavtabsmenu/ProposalsListFragment.java
+++ app/src/main/java/om/gov/ita/drawerbottomnavtabsmenu/ProposalsListFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -47,12 +50,23 @@ public class ProposalsListFragment extends Fragment {
         addProposalFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(rootView, "Fab Action", Snackbar.LENGTH_SHORT).show();
+                showDialog();
             }
         });
 
 
         // Inflate the layout for this fragment
         return rootView;
+    }
+
+    public void showDialog() {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        NewProposalDialogFragment newProposalDialogFragment = new NewProposalDialogFragment();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // For a little polish, specify a transition animation
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//        // To make it fullscreen, use the 'content' root view as the container
+//        // for the fragment, which is always the root view for the activity
+        transaction.add(android.R.id.content, newProposalDialogFragment).commit();
     }
 }
